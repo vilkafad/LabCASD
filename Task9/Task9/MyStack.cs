ecc16265dc0da17150f297fd5ba84ce037ecd1fe
vilkafad <161117795+vilkafad@users.noreply.github.com>
2024-10-20 14:10:50 +0300

    laba9

---
+++ Task9/Task9/MyStack.cs
@@ -0,0 +1,270 @@
+﻿namespace Task9
+{
+
+
+    public class MyVector<T>
+    {
+        private T[] elementData;
+        private int elementCount;
+        private int capacityIncrement;
+        public MyVector(int initialCapacity, int initialCapacityIncrement)
+        {
+            elementData = new T[initialCapacity];
+            elementCount = initialCapacity;
+            capacityIncrement = initialCapacityIncrement;
+        }
+        public MyVector(int initialCapacity)
+        {
+            elementData = new T[initialCapacity];
+            elementCount = 0;
+            capacityIncrement = 0;
+        }
+        public MyVector()
+        {
+            elementData = null;
+            elementCount = 10;
+            capacityIncrement = 0;
+        }
+        public MyVector(T[] array)
+        {
+            elementData = new T[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                elementData[i] = array[i];
+            elementCount = array.Length;
+            capacityIncrement = 0;
+        }
+        public void Add(T value)
+        {
+            if (elementCount == elementData.Length)
+                Resize();
+            elementData[elementCount++] = value;
+        }
+        private void Resize()
+        {
+            T[] newArray;
+            if (capacityIncrement != 0)
+                newArray = new T[(int)(elementData.Length * capacityIncrement)];
+            else newArray = new T[(int)(elementData.Length * 2)];
+            for (int i = 0; i < elementCount; i++)
+                newArray[i] = elementData[i];
+            elementData = newArray;
+        }
+        public void AddAll(params T[] array)
+        {
+            for (int i = 0; i < array.Length; i++) Add(array[i]);
+        }
+        public int Count => elementCount;
+        public void Clear()
+        {
+            elementCount = 0;
+            elementData = null;
+        }
+        public bool Contains(object value)
+        {
+            foreach (object element in elementData)
+                if (element.Equals(value)) return true;
+            return false;
+        }
+        public bool ContainsAll(T[] array)
+        {
+            bool[] b = new bool[array.Length];
+            int index = 0;
+            foreach (T item in array)
+            {
+                for (int i = 0; i < elementCount; i++)
+                {
+                    if (item.Equals(elementData[i]))
+                    {
+                        b[index] = true;
+                        if (index != b.Length) index++;
+                    }
+                }
+            }
+            foreach (bool element in b)
+            {
+                if (!element) return false;
+            }
+            return true;
+        }
+        public T Get(int index) => elementData[index];
+        public bool Empty()
+        {
+            return elementCount == 0;
+        }
+        public T Remove(object item)
+        {
+            int index = -1;
+            for (int i = 0; i < elementCount; i++)
+                if (item.Equals(elementData[i])) { index = i; break; }
+            if (index != -1)
+            {
+                T element = elementData[index];
+                for (int i = index; i < elementCount - 1; i++)
+                {
+                    elementData[i] = elementData[i + 1];
+                }
+                elementCount--;
+                return element;
+            }
+            return default;
+        }
+        public void Remove(params T[] element)
+        {
+            foreach (object obj in element)
+            {
+                int i = 0;
+                while (i < elementCount)
+                {
+                    if (obj.Equals(elementData[i]))
+                    {
+                        for (int j = i; j < elementCount - 1; j++)
+                            elementData[j] = elementData[j + 1];
+                        elementCount--;
+                    }
+                    i++;
+                }
+            }
+        }
+        public void Retain(params T[] array)
+        {
+            T[] newArray = new T[elementCount];
+            int newSize = 0;
+            for (int i = 0; i < elementCount; i++)
+            {
+                foreach (T obj in array)
+                    if (obj.Equals(elementData[i])) newArray[newSize++] = elementData[i];
+            }
+            elementCount = newSize;
+            elementData = newArray;
+        }
+        public T[] toArray()
+        {
+            T[] answerArray = new T[elementCount];
+            for (int i = 0; i < elementCount; i++) answerArray[i] = elementData[i];
+            return answerArray;
+        }
+        public T[] ToArray(T[] array)
+        {
+            if (array == null)
+                array = new T[elementCount];
+            for (int i = 0; i < elementCount; i++)
+                array[i] = elementData[i];
+            return array;
+        }
+        public void addAll(int index, params T[] array)
+        {
+            if (index > elementCount)
+            {
+                AddAll(array);
+                return;
+            }
+            else
+            {
+                int ind = 0;
+                T[] newArray = new T[elementCount + array.Length];
+                for (int i = 0; i < index; i++)
+                    newArray[i] = elementData[i];
+                for (int i = index; i < index + array.Length; i++)
+                    newArray[i] = array[ind++];
+                for (int i = index + array.Length; i < elementCount; i++)
+                    newArray[i] = elementData[i];
+                elementData = newArray;
+                elementCount = newArray.Length;
+            }
+        }
+        public int IndexOf(T element)
+        {
+            for (int i = 0; i < elementCount; i++)
+                if (element.Equals(elementData[i])) return i;
+            return -1;
+        }
+        public int LastIndexOf(T element)
+        {
+            int index = -1;
+            for (int i = 0; i < elementCount; i++)
+                if (element.Equals(elementData[i])) index = i;
+            return index;
+        }
+        public T Remove(int index)
+        {
+            if ((index < 0) || (index >= elementCount)) throw new ArgumentOutOfRangeException("index");
+            T element = elementData[index];
+            for (int i = index; i < elementCount - 1; i++)
+                elementData[i] = elementData[i + 1];
+            elementCount--;
+            return element;
+        }
+        public void Set(int index, T element)
+        {
+            if (index >= elementCount || index < 0) throw new ArgumentOutOfRangeException("index");
+            if (element == null) throw new ArgumentNullException(element.ToString());
+            elementData[index] = element;
+        }
+        public MyVector<T> SubList(int fromIndex, int toIndex)
+        {
+            if (fromIndex < 0 || fromIndex >= elementCount) throw new ArgumentOutOfRangeException("fromindex");
+            if (toIndex < 0 || toIndex >= elementCount) throw new ArgumentOutOfRangeException("toindex");
+            MyVector<T> newArray = new MyVector<T>(toIndex - fromIndex, 10);
+            for (int i = 0; i < newArray.elementCount; i++)
+                newArray.Set(i, elementData[fromIndex + i]);
+            return newArray;
+        }
+        public T FirstElement() => elementData[0];
+        public T LastElement() => elementData[elementCount - 1];
+        public void RemoveElement(int position) => Remove(position);
+        public void RemoveRange(int begin, int end)
+        {
+            if ((begin < 0) || (begin >= elementCount)) throw new ArgumentOutOfRangeException("begin out of range");
+            if ((end < 0) || (end >= elementCount)) throw new ArgumentOutOfRangeException("end out of range");
+            T[] newArray = new T[elementCount - (end - begin)];
+            int index = 0;
+            for (int i = begin; i < end; i++)
+            {
+                newArray[index++] = elementData[i];
+            }
+            this.Remove(newArray);
+        }
+    }
+
+
+public class MyStack<T> : MyVector<T>
+    {
+        private MyVector<T> stack;
+        private int top;
+        public MyStack()
+        {
+            top = -1;
+            stack = new MyVector<T>(1);
+        }
+        public void Push(T item)
+        {
+            top++;
+            stack.Add(item);
+        }
+        public void Pop()
+        {
+            stack.Remove(top);
+            top--;
+        }
+        public T Peek()
+        {
+            if (top == -1) throw new Exception("Stack is empty");
+            else return stack.Get(top);
+        }
+        public bool Empty()
+        {
+            return stack.Count == 0;
+        }
+        public int Search(T item)
+        {
+            if (stack.IndexOf(item) == -1) return -1;
+            return top - stack.IndexOf(item) + 1;
+        }
+        public int Top() { return top; }
+        public void print()
+        {
+            for (int i = 0; i <= top; i++)
+                Console.WriteLine(stack.Get(i));
+        }
+    }
+}
---
+++ Task9/Task9/Program.cs
@@ -0,0 +1,246 @@
+﻿using Task9;
+
+public class ReversePolishNotation
+{
+    static public int WeightOperator(string opertator)
+    {
+        switch (opertator)
+        {
+            case "+":
+                return 2;
+            case "-":
+                return 2;
+            case "*":
+            case "/":
+            case "//":
+                return 3;
+            case "^":
+                return 4;
+            case "sqrt":
+            case "ln":
+            case "cos":
+            case "sin":
+            case "tg":
+            case "ctg":
+            case "abs":
+            case "log":
+            case "min":
+            case "max":
+            case "mod":
+            case "exp":
+            case "trunc":
+                return 5;
+            case "%":
+                return 6;
+            default: return 0;
+        }
+    }
+    private static double Execute(string op, params double[] x) => op switch
+    {
+        "+" => x[0] + x[1],
+        "-" => x[0] - x[1],
+        "*" => x[0] * x[1],
+        "/" => x[0] / x[1],
+        "^" => Math.Pow(x[0], x[1]),
+        "//" => Math.Floor(x[0] / x[1]),
+        "exp" => Math.Exp(x[0]),
+        "sqrt" => Math.Sqrt(x[0]),
+        "abs" => Math.Abs(x[0]),
+        "ln" => Math.Log(x[0]),
+        "log" => Math.Log10(x[0]),
+        "sin" => Math.Sin(x[0]),
+        "cos" => Math.Cos(x[0]),
+        "tg" => Math.Tan(x[0]),
+        "ctg" => 1 / Math.Tan(x[0]),
+        "max" => x[0] > x[1] ? x[0] : x[1],
+        "min" => x[0] < x[1] ? x[0] : x[1],
+        "trunc" => Math.Truncate(x[0]),
+        "mod" => (int)x[0] % (int)x[1],
+        _ => 0
+    };
+    private static string GetStringNumber(string expr, ref int pos)
+    {
+        string output = "";
+        for (; pos < expr.Length; pos++)
+        {
+            if (Char.IsDigit(expr[pos]) || expr[pos] == ',')
+            {
+                output += expr[pos];
+            }
+            else
+            {
+                pos--;
+                break;
+            }
+        }
+        return output;
+    }
+    private static string GetStringText(string expr, ref int pos)
+    {
+        string output = "";
+        for (; pos < expr.Length; pos++)
+        {
+            if (Char.IsLetter(expr[pos]) || expr[pos] == '/')
+            {
+                output += expr[pos];
+            }
+            else
+            {
+                pos--;
+                break;
+            }
+        }
+        return output;
+    }
+    public static string ToPostfix(string infixExpr)
+    {
+        string postFix = "";
+        MyStack<string> stack = new MyStack<string>();
+        for (int i = 0; i < infixExpr.Length; i++)
+        {
+            //1
+            if (Char.IsDigit(infixExpr[i]))
+            {
+                postFix += GetStringNumber(infixExpr, ref i) + " ";
+            }
+            else if (Char.IsLetter(infixExpr[i]) || (infixExpr[i] == '/' && infixExpr[i + 1] == '/'))
+            {
+                //2
+                string name = "";
+                name += GetStringText(infixExpr, ref i);
+                if (WeightOperator(name) != 0)
+                {
+                    //b
+                    while (!stack.Empty() && (WeightOperator(stack.Peek()) >= WeightOperator(name)))
+                    {
+                        postFix += stack.Peek();
+                        stack.Pop();
+                    }
+                    //a
+                    stack.Push(name);
+                }
+            }
+            else if (WeightOperator(Convert.ToString(infixExpr[i])) != 0)
+            {
+                //b
+                while (!stack.Empty() && (WeightOperator(stack.Peek()) >= WeightOperator(Convert.ToString(infixExpr[i]))))
+                {
+                    postFix += stack.Peek() + " ";
+                    stack.Pop();
+                }
+                //a
+                stack.Push(Convert.ToString(infixExpr[i]));
+            }
+            //3
+            else if (infixExpr[i] == '(' && i + 1 != infixExpr.Length && infixExpr[i + 1] == '-')
+            {
+                if (Char.IsDigit(infixExpr[i + 2]))
+                {
+                    i += 2;
+                    string n = GetStringNumber(infixExpr, ref i);
+                    postFix += Convert.ToString('-') + n + " ";
+                    i += 1;
+                }
+
+            }
+            else if (infixExpr[i] == '(') stack.Push(Convert.ToString(infixExpr[i]));
+
+            //4
+            else if (infixExpr[i] == ')')
+            {
+                while (stack.Peek() != "(")
+                {
+                    postFix += stack.Peek() + " ";
+                    stack.Pop();
+                }
+                stack.Pop();
+            }
+        }
+        while (!stack.Empty())
+        {
+            postFix += stack.Peek() + " ";
+            stack.Pop();
+        }
+        return postFix;
+    }
+    public static double Calc(string postFix)
+    {
+        string name = "";
+        MyStack<double> component = new MyStack<double>();
+        for (int i = 0; i < postFix.Length; i++)
+        {
+            name = "";
+            if (Char.IsDigit(postFix[i]))
+            {
+                string number = GetStringNumber(postFix, ref i);
+                component.Push(Convert.ToDouble(number));
+            }
+            else if (Char.IsLetter(postFix[i]) || (postFix[i] == '/' && postFix[i + 1] == '/'))
+            {
+                name += GetStringText(postFix, ref i);
+            }
+            else if ((i + 1 < postFix.Length || i + 2 < postFix.Length) && postFix[i] == '-')
+            {
+                if (Char.IsDigit(postFix[i + 2]))
+                {
+                    i += 1;
+                    string number;
+                    number = GetStringNumber(postFix, ref i);
+                    double x = Convert.ToDouble(number);
+                    component.Push(-x);
+                    i += 1;
+                }
+            }
+            else if (WeightOperator(Convert.ToString(postFix[i])) != 0)
+            {
+                double y = component.Peek();
+                component.Pop();
+                double x = component.Peek();
+                component.Pop();
+                component.Push(Execute(Convert.ToString(postFix[i]), x, y));
+            }
+            if (WeightOperator(name) != 0 && name == "//")
+            {
+                double y = component.Peek();
+                component.Pop();
+                double x = component.Peek();
+                component.Pop();
+                component.Push(Execute(name, x, y));
+            }
+
+            if (WeightOperator(name) != 0)
+            {
+
+                if (WeightOperator(name) == 5 && (name != "mod" || name != "min" || name != "max"))
+                {
+                    double y = component.Peek();
+                    component.Pop();
+                    component.Push(Execute(Convert.ToString(name), y));
+                }
+                else if (WeightOperator(name) == 5 && (name == "mod" || name == "min" || name == "max"))
+                {
+                    double y = component.Peek();
+                    component.Pop();
+                    double x = component.Peek();
+                    component.Pop();
+                    component.Push(Execute(Convert.ToString(name), x, y));
+                }
+            }
+        }
+        return component.Peek();
+    }
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Пожалуйста, передайте параметры.");
+            return;
+        }
+        string joinedArgs = string.Join(",", args);
+        string g = ToPostfix(joinedArgs);
+        Console.WriteLine(g);
+        double res = Calc(g);
+        Console.WriteLine(res);
+    }
+
+}
